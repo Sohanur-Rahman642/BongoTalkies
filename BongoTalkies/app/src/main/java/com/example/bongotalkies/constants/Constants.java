29af04d06b89fb65ea2869597d08a1824276a4c9
Sohanur-Rahman642 <rahman.sohanur@northsouth.edu>
2022-05-22 00:55:47 +0600

    add: added Constant file

---
+++ BongoTalkies/app/src/main/java/com/example/bongotalkies/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.example.bongotalkies.constants;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+
+    public static final String API_KEY = "c37d3b40004717511adb2c1fbb15eda4";
+
+    public static final String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original/";
+
+}
