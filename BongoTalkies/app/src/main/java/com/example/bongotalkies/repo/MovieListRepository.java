e875487de0842227085770d664d9d428c292ff34
Sohanur-Rahman642 <rahman.sohanur@northsouth.edu>
2022-05-22 01:07:31 +0600

    add: completed calling listofmovie fetching api in the repo

--- BongoTalkies/app/src/main/java/com/example/bongotalkies/repo/MovieListRepository.java
+++ BongoTalkies/app/src/main/java/com/example/bongotalkies/repo/MovieListRepository.java
@@ -1,15 +1,41 @@
 package com.example.bongotalkies.repo;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.bongotalkies.application.BongoTalkies;
+import com.example.bongotalkies.model.MovieModel;
+import com.example.bongotalkies.model.response.MovieListResponse;
 import com.example.bongotalkies.server.ApiInterface;
 
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MovieListRepository {
 
     private final String TAG = getClass().getSimpleName();
 
-    ApiInterface apiInterface = BongoTalkies.getRetrofitClient().create(ApiInterface.class);
+    public MutableLiveData<List<MovieModel>> getListOfMovies(String apiKey, String language, int page){
 
+        final MutableLiveData<List<MovieModel>> mutableLiveData = new MutableLiveData<>();
 
+        ApiInterface apiInterface = BongoTalkies.getRetrofitClient().create(ApiInterface.class);
+
+        apiInterface.getListOfMovies(apiKey, language, page).enqueue(new Callback<MovieListResponse>() {
+            @Override
+            public void onResponse(Call<MovieListResponse> call, Response<MovieListResponse> response) {
+                mutableLiveData.setValue(response.body().getResults());
+            }
+
+            @Override
+            public void onFailure(Call<MovieListResponse> call, Throwable t) {
+                mutableLiveData.setValue(null);
+            }
+        });
+
+        return mutableLiveData;
+
+    }
 }
